3ff6179aa867bae67ce9a218c71e9561d2b1a724
henryadebayo <henryadebayo250@gmail.com>
2022-02-27 22:29:13 +0100

    added bloc

--- lib/cubit/coingecko_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-import 'package:symfonia/models/crypto_model.dart';
-
-part 'coingecko_state.dart';
-
-class CoingeckoCubit extends Cubit<CoingeckoState> {
-  CoingeckoCubit() : super(CoingeckoInitial());
-}
